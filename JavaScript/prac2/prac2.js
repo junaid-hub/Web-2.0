3447386f2735f3a229588fe58c9a14a00206fdb0
junaid-hub <junaidnazir7501@gmail.com>
2022-12-06 19:22:17 +0500

    var,let,const keywords

---
+++ JavaScript/prac2/prac2.js
@@ -0,0 +1,34 @@
+//alert('Saying Hello from a separate file')
+//console.log('Hello World')
+
+
+// let statuss = 'Junaid Nazir';
+
+// if (statuss === 'Junaid Nazir'){
+//     console.log('Hello World!');
+// } else {
+//     console.log('World Hello!!');
+// }
+
+
+//prompt('Enter Your Name')
+
+//// generating random number between 1 and 100
+//// changing it to integer
+//console.log(Math.floor(Math.random() * 100))
+
+//// var -> Global Scope
+//// let -> Block Scope
+
+let a = 5
+
+if (a == 5){
+    const new_var = 'Hello World'            
+}
+
+// This will give error beacuse const new_var is declared inside 
+// block whereas we are trying to access it outside the block
+console.log(new_var)
+
+
+
